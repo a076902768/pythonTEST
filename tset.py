9f59264305ab670e20d25778b9a64d6e54a315ef
Wei Bo Zheng <47736792+a076902768@users.noreply.github.com>
2020-10-28 17:31:55 +0800

    update

---
+++ tset.py
@@ -0,0 +1,6 @@
+#參考: https://www.coderbridge.com/series/c471d97bb201460ab137c5e4955987df/posts/e1755ac2099b430291ffb7a8b61f169e
+import tkinter as tk #匯入tkinter模組
+
+win = tk.Tk() #建立主視窗物件
+win.geometry('640x480') #設定主視窗預設尺寸為640*480
+win.resizable(False,False)
